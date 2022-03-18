488af7d1839f3375c5346d7e98248d3adc5dc892
alex_mosiakin <mosyakin.a@flat12.ru>
2022-03-18 17:07:17 +0300

    Add sort and search functionality, also add some UI's stuff(input, select) and general styles

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@ import React, { useEffect, useState } from "react";
 import PokemonList from "./components/PokemonList";
 import pokeService from "./service/pokeService";
 import "../src/styles/App.css"
+import Select from './components/UI/Select/Select'
 
 function App() {
-  const offset = useState(0);
-  const limit = useState(10);
-  const [pokemons, setPokemons] = useState([]);
-
-  const pokemonsFetch = async () => {
-    const responce = await pokeService.getAllPoke(offset, limit);
-    setPokemons([...pokemons, ...responce.data.results]);
-  }
-  useEffect(() => {
-    pokemonsFetch();
-  },[])
 
   return (
     <div className="pokemon-app">
       <h1>PokeLand</h1>
-      <div className="pokemon-list-wrapper">
-        {
-          pokemons.map(pokemon => 
-            <PokemonList key={pokemon.name} url={pokemon.url} />
-          )
-        }
-      </div>
+      <PokemonList/>
     </div>
   );
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,38 +1,73 @@
-import React, {useState, useEffect} from "react";
-import pokeService from "../service/pokeService";
+import React, {useState, useEffect, useMemo} from "react";
 import '../styles/PokemonList.css'
+import PokemonItem from "./PokemonItem";
+import Select from "./UI/Select/Select";
+import pokeService from "../service/pokeService";
+import Input from "./UI/Input/Input";
 
-function PokemonList(props) {
-    const url = props.url
-    const [pokemon, setPokemon] = useState({
-        sprites:{
-            other:{
-                dream_world:{
-                    front_default: 0
-                }
-            }
-        }
-    });
+function PokemonList() {
+    const offset = useState(0);
+    const limit = useState(10);
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [pokemons, setPokemons] = useState([]);
     const [isLoading, setLoading] = useState(false);
 
-    const pokemonFetch = async () => {
-        setLoading(true);
-        const responce = await pokeService.getCurrentPoke(url);
-        setPokemon({...pokemon, ...responce.data});
-        setLoading(false);
-    }
+    const sortedPokemons = useMemo(() => {
+        if(selectedSort){
+            return [...pokemons].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return pokemons;
+    },[selectedSort, pokemons])
 
+    const sortedAndSearchedPokemons = useMemo(() => {
+        return sortedPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchQuery));
+    },[searchQuery, sortedPokemons])
+  
+    const pokemonsFetch = async () => {
+      setLoading(true);
+      const responce = await pokeService.getAllPoke(offset, limit);
+      setPokemons([...pokemons, ...responce.data.results]);
+      setLoading(false);
+    }
     useEffect(() => {
-        pokemonFetch(url)
+      pokemonsFetch();
     },[])
+
+    const sortPokemons = (sort) => {
+        setSelectedSort(sort);
+    }
 
     return(
         isLoading ? 
         <h5>Loading...</h5>
         :
-        <div className="pokemon-list-item">
-            <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
-            <p>{pokemon.name}</p>
+        <div className="pokemon-list-wrapper">
+        <div className="controls-wrapper">
+            <Input 
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value.toLowerCase())}
+                placeholder="Search..."
+            />
+            <Select 
+                value={selectedSort}
+                onChange={sortPokemons}
+                defaultValue='Sort'
+                options={[
+                {value: 'name', name: 'By name'},
+                {value: 'url', name: 'By id'},
+                ]}
+            />
+        </div>
+        {sortedAndSearchedPokemons.length ? 
+        <div className="pokemon-list-container">
+            {sortedAndSearchedPokemons.map(pokemon => 
+                <PokemonItem key={pokemon.name} url={pokemon.url}/>
+            )}
+        </div>
+        :
+        <h5 className="no-pokemons">No pokemons :(</h5>
+        }
         </div>
     )
 }
